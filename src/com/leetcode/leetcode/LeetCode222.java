b258cb13999e05a6fe23869fdb3e4fe19c8085d7
bigmud <bigmudhaijian@gmail.com>
2021-11-30 16:47:48 +0800

    LeetCode222

--- src/com/leetcode/leetcode/LeetCode222.java
+++ src/com/leetcode/leetcode/LeetCode222.java
@@ -1,7 +1,27 @@
 package com.leetcode.leetcode;
 
+import com.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode222 {
     public static void main(String[] args){
-        
+
+    }
+
+    public static int countNodes(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        test(root, result);
+        return result.size();
+    }
+
+    public static void test(TreeNode root, List<Integer> result) {
+        if(root == null){
+            return;
+        }
+        result.add(1);
+        test(root.left, result);
+        test(root.right, result);
     }
 }
